0e1907dc24dec0531e56bde5455a23b80001c9e1
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2022-04-18 20:28:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 class cliente{
     nome;
     cpf;
+}
+
+class contaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor <= 0 ) {
+            return;
+        }
+        this._saldo += valor;
+        }
 }
 
 const cliente1 = new cliente();
@@ -10,12 +27,17 @@ const cliente2 = new cliente();
 
 cliente1.nome = "Fillipe";
 cliente1.cpf = 12312312312;
-cliente1.agencia = 0001;
-cliente1.saldo = 0;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 12312312313;
-cliente2.agencia = 0001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteFillipe = new contaCorrente();
+contaCorrenteFillipe.agencia = 0001;
+
+ contaCorrenteFillipe.depositar(100);
+ contaCorrenteFillipe.depositar(100);
+ contaCorrenteFillipe.depositar(100);
+
+ const valorSacado = contaCorrenteFillipe.sacar(50);
+ console.log(valorSacado);
+ console.log(contaCorrenteFillipe);
