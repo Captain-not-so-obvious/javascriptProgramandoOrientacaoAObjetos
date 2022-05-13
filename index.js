398f25b208887a9b69f23b70e0a09f79534db331
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2022-05-13 20:50:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Fillipe", 12312312312);
-const cliente2 = new Cliente("Alice", 12312312313);
 
-let numeroDeContas = 0;
 
-const contaCorrenteFillipe = new ContaCorrente(1001, cliente1);
+const contaCorrenteFillipe = new Conta(0, cliente1, 1001);
 contaCorrenteFillipe.depositar(500);
+contaCorrenteFillipe.sacar(100);
 
-const conta2 = new ContaCorrente(1001, cliente2);
-contaCorrenteFillipe.transferir(200, conta2);
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
+console.log(contaPoupanca);
 console.log(contaCorrenteFillipe);
