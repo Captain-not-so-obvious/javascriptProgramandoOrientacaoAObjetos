80b107001cc67c5ff877fe633a220be4323f4a42
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2022-05-16 22:39:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Fillipe", 12312312312);
 
 
-const contaCorrenteFillipe = new Conta(0, cliente1, 1001);
+const contaCorrenteFillipe = new ContaCorrente(cliente1, 1001);
 contaCorrenteFillipe.depositar(500);
 contaCorrenteFillipe.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteFillipe);
