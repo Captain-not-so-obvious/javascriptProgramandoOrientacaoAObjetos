1e051458be12dafd611bd4d025004ad64e00f965
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2022-05-17 22:18:39 -0300

    Update Conta.js

--- Conta.js
+++ Conta.js
@@ -1,8 +1,12 @@
 export class Conta {
     constructor(saldoInicial, cliente, agencia){
+        if(this.constructor == Conta) {
+            throw new Error ("Você não deveria instanciar um objeto do tipo conta!");
+        
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+        }
     }
 
     set cliente(novoValor) {
@@ -23,8 +27,7 @@ export class Conta {
 
     
     sacar(valor) {
-        let taxa = 1;
-        return this._sacar(valor, taxa);
+        throw new Error("O método sacar da conta é abstrato");
     }
 
     _sacar(valor, taxa) {
